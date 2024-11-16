58a3c7d874b53e4a24b507cad4b606ba6eff7033
ZEROXOWNER <vivek2006kumar1011@gmail.com>
2024-11-16 13:24:35 +0530

    Update main.php

--- main.php
+++ main.php
@@ -48,10 +48,10 @@ $messagesec",
 	'reply_to_message_id'=> $message_id,
     'reply_markup'=>json_encode(['inline_keyboard' => [
         [
-          ['text' => "💠 Created By 💠", 'url' => "t.me/ninjanaveen"]
+          ['text' => "💠 Created By 💠", 'url' => "t.me/zerox_owner"]
         ],
         [
-          ['text' => "💎 Source Code 💎", 'url' => "t.me/IndianBotsChat"]
+          ['text' => "💎 Source Code 💎", 'url' => "t.me/zerox_owner"]
         ],
       ], 'resize_keyboard' => true])
         
